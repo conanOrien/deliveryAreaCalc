50f29602f1c677821b25603c0cd95e93aab46833
conanOrien <ow5@njit.edu>
2017-02-28 14:16:23 -0500

    First commit

---
+++ deliveryArea.php
@@ -0,0 +1,25 @@
+<?php
+    $url ="https://maps.googleapis.com/maps/api/distancematrix/";
+    $orig ="";
+    $destination ="";
+    $key ="";
+
+    function calculateDistance()
+    {
+
+    }
+    class dist_calc
+    {
+    	
+    }
+
+
+  ?>
+  <html>
+    <div id="formDiv" style="border:solid black 5px">
+        <form id="dataDiv">
+            Enter your Address:<br>
+            <input type="text" id="delivAdd" name="delivAdd" style="margin-left:5px"></input>
+        </form>
+    </div>
+  </html>
