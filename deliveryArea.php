bd293edb6e9265550364490e30b9a4a8e6e9af4b
conanOrien <ow5@njit.edu>
2017-03-02 14:17:00 -0500

    tweaked display, next work on underlying function calls

--- deliveryArea.php
+++ deliveryArea.php
@@ -2,6 +2,7 @@
     $url ="https://maps.googleapis.com/maps/api/distancematrix/";
     $orig ="";
     $destination ="";
+    $delivDist=5;
     $key ="";
 
     function calculateDistance()
@@ -15,11 +16,17 @@
 
 
   ?>
+  <style>
+  </style>
+
   <html>
-    <div id="formDiv" style="border:solid black 5px">
-        <form id="dataDiv">
-            Enter your Address:<br>
-            <input type="text" id="delivAdd" name="delivAdd" style="margin-left:5px"></input>
-        </form>
-    </div>
+    <head>
+        <div id="formDiv" style="border:solid black 5px; position=relative; height:70px; width:300px">
+            <form id="dataDiv" style="padding:10px; margin-left:10px">
+                Enter your Address:<br>
+                <input type="text" id="delivAdd" name="delivAdd" style="margin-left:5px"></input>
+                <button onclick=calculateDistance()>Submit</button>
+            </form>
+        </div>
+    </head>
   </html>
